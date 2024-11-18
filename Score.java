a8f589eb4946bfea82e64b6606865acae5c9e042
carolinaroque05 <up202305062@up.pt>
2024-11-18 23:15:37 +0000

    Aclasse score inicializada com a pontuaçao do jogador

---
+++ Score.java
@@ -0,0 +1,15 @@
+public class Score {
+    private int score;
+
+    public Score() {
+        score = 0;
+    }
+
+    public void addScore(int score){
+        this.score += score;
+    }
+
+    public int getScore(){
+        return score;
+    }
+}
