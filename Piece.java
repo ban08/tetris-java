53a8d011dad5e115220e3d9fdfd64f02419e1ed5
carolinaroque05 <up202305062@up.pt>
2024-11-18 22:53:41 +0000

    atribuição do método color nas pieces

--- Piece.java
+++ Piece.java
@@ -2,12 +2,14 @@ public class Piece {
     private final int[][] shape; //forma da peça
     private int x;
     private int y;
+    private final int color;
 
 
-    public Piece(int[][] shape) {
+    public Piece(int[][] shape, int color) {
         this.shape = shape;
         this.x = 0; // Inicializa na posição padrão
         this.y = 0;
+        this.color = color;
 
     }
     public int[][] getShape() {
@@ -23,6 +25,10 @@ public class Piece {
         return y;
     }
 
+    public int getColor() {
+        return color;
+    }
+
     public void move(int posX, int posY){
         this.x +=posX;
         this.y += posY;
@@ -36,55 +42,55 @@ public class Piece {
         return shape.length; //numero de linhas ocupadas pela peça
     }
 
-    public static Piece createPiece(int[][] shape){
-        return new Piece(shape);
+    public static Piece createPiece(int[][] shape, int color){
+        return new Piece(shape, color);
     }
-    public static Piece pieceT(){
+    public static Piece pieceT(int color){
         int[][] tShape = {
                 {0, 1, 0},
                 {1, 1, 1}
         };
-        return createPiece(tShape);
+        return createPiece(tShape, color);
     }
 
-    public static Piece pieceL(){
+    public static Piece pieceL(int color){
         int[][] lShape = {
                 {1, 0},
                 {1, 0},
                 {1, 1}
         };
-        return createPiece(lShape);
+        return createPiece(lShape, color);
     }
 
-    public static Piece pieceSquare(){
+    public static Piece pieceSquare(int color){
         int[][] squareShape = {
                 {1, 1},
                 {1, 1}
         };
-        return createPiece(squareShape);
+        return createPiece(squareShape, color);
     }
 
-    public static Piece pieceZ(){
+    public static Piece pieceZ(int color){
         int[][] zShape = {
                 {1, 1, 0},
                 {0, 1, 1}
         };
-        return createPiece(zShape);
+        return createPiece(zShape, color);
     }
 
-    public static Piece pieceS(){
+    public static Piece pieceS(int color){
         int[][] sShape = {
                 {0, 1, 1},
                 {1, 1, 0}
         };
-        return createPiece(sShape);
+        return createPiece(sShape, color);
     }
 
-    public static Piece pieceI(){
+    public static Piece pieceI(int color){
         int[][] iShape = {
                 {1, 1, 1, 1},
         };
-        return createPiece(iShape);
+        return createPiece(iShape, color);
     }
 
 }
