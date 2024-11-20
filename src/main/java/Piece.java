ced61a5be559c182b59586a8f3328d76de76bebb
carolinaroque05 <up202305062@up.pt>
2024-11-20 09:25:33 +0000

    atualize piece class

--- src/main/java/Piece.java
+++ src/main/java/Piece.java
@@ -1,98 +1,56 @@
+import com.googlecode.lanterna.TextColor;
+
 public class Piece {
-    private final int[][] shape; //forma da peça
-    private int x;
-    private int y;
-    private final int color;
+    private final char[][] shape; //forma da peça
+    private TextColor color;
 
 
-    public Piece(int[][] shape, int color) {
+    public Piece(char[][] shape, TextColor color) {
         this.shape = shape;
-        this.x = 0; // Inicializa na posição padrão
-        this.y = 0;
         this.color = color;
 
     }
-    public int[][] getShape() {
+    public char[][] getShape() {
         return shape;
     }
 
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public int getColor() {
+    public TextColor getColor() {
         return color;
     }
 
-    public void move(int posX, int posY){
-        this.x +=posX;
-        this.y += posY;
-    }
+    public static Piece randomShape(){
+        char[][] shape;
+        TextColor color;
 
-    public int getWith(){
-        return shape[0].length; //numero de colunas ocupadas pela peça na linha
+        switch ((int) (Math.random() * 5)) {
+            case 0:
+                shape = new char[][]{{'O', 'O'}, {'O', 'O'}};
+                color = TextColor.ANSI.YELLOW;
+                break;
+            case 1:
+                shape = new char[][]{{'I', 'I', 'I', 'I'}};
+                color = TextColor.ANSI.CYAN;
+                break;
+            case 2:
+                shape = new char[][]{{' ', 'T', ' '}, {'T', 'T', 'T'}};
+                color = TextColor.ANSI.MAGENTA;
+                break;
+            case 3:
+                shape = new char[][]{{'J', ' '}, {'J', ' '}, {'J', 'J'}};
+                color = TextColor.ANSI.BLUE;
+                break;
+            case 4:
+                shape = new char[][]{{'L', ' '}, {'L', ' '}, {'L', 'L'}};
+                color = TextColor.ANSI.RED;
+                break;
+            default:
+                shape = new char[][]{{' ', 'I', ' '}, {' ', 'I', ' '}, {' ', 'I', ' '}}; // Forma do "I" (vertical)
+                color = TextColor.ANSI.GREEN;
+        }
+        return new Piece(shape, color); // Retorna a peça aleatória criada
     }
-
-    public int getHeight(){
-        return shape.length; //numero de linhas ocupadas pela peça
-    }
-
-    public static Piece createPiece(int[][] shape, int color){
-        return new Piece(shape, color);
-    }
-    public static Piece pieceT(int color){
-        int[][] tShape = {
-                {0, 1, 0},
-                {1, 1, 1}
-        };
-        return createPiece(tShape, color);
-    }
-
-    public static Piece pieceL(int color){
-        int[][] lShape = {
-                {1, 0},
-                {1, 0},
-                {1, 1}
-        };
-        return createPiece(lShape, color);
-    }
-
-    public static Piece pieceSquare(int color){
-        int[][] squareShape = {
-                {1, 1},
-                {1, 1}
-        };
-        return createPiece(squareShape, color);
-    }
-
-    public static Piece pieceZ(int color){
-        int[][] zShape = {
-                {1, 1, 0},
-                {0, 1, 1}
-        };
-        return createPiece(zShape, color);
-    }
-
-    public static Piece pieceS(int color){
-        int[][] sShape = {
-                {0, 1, 1},
-                {1, 1, 0}
-        };
-        return createPiece(sShape, color);
-    }
-
-    public static Piece pieceI(int color){
-        int[][] iShape = {
-                {1, 1, 1, 1},
-        };
-        return createPiece(iShape, color);
-    }
-
-
 
 }
+
+
+
