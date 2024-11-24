d3d5ad3e6f24ca043cfa690da2edae70522bff3f
raulnasoliveira <raulnasoliveira@gmail.com>
2024-11-24 22:59:05 +0000

    added tests to PieceSelector and Score

---
+++ src/test/java/ScoreTest.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ScoreTest {
+
+    @Test
+    void testInitialScore() {
+        Score score= new Score();
+        int initpoints= score.getPoints();
+        assertEquals(0,initpoints);
+
+    }
+    @Test
+    void testOnlineCleared() {
+        Score score= new Score();
+        score.onLineCleared(2);
+        score.onLineCleared(1);
+        score.onLineCleared(3);
+        assertEquals(600,score.getPoints());
+    }
+    @Test
+    void testOnGameOver() {
+        Score score= new Score();
+        score.onGameOver();
+    }
+}
